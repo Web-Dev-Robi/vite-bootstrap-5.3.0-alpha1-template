817ebe101506231018d22b8812adc91bd24d30ca
desiredstate2021 <desiredshellstate@gmail.com>
2023-02-04 20:03:12 +0100

    Pre-compiled Bootstrapv5.3.0-alpha1 for react/vite

---
+++ README.md
@@ -0,0 +1,5 @@
+Bootstrap 5.3.0-alpha1 pre-compiled for react/vite.
+
+1-Clone repository
+2-npm install in root folder
+3-enjoy
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+function App() {
+ return (
+    <div className="App">
+      <div class="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button class="btn btn-primary">Primary button</button>
+      </div>
+    </div>
+  );
+}
+
+export default App
